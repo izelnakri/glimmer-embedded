adf2c5468b60fbbd32f4eae557fa92f4bdc3b7b7
Izel Nakri <contact@izelnakri.com>
2021-05-20 03:24:34 +0200

    examples are added

---
+++ examples/old-aot.js
@@ -0,0 +1,35 @@
+// NOTE: old simple embed API:
+import { Component, Context } from '@glimmer/opcode-compiler';
+import { artifacts } from '@glimmer/program';
+import { precompile } from '@glimmer/compiler';
+import createHTMLDocument from '@simple-dom/document';
+import { AotRuntime, renderAot } from '@glimmer/runtime';
+import Serializer from '@simple-dom/serializer';
+import voidMap from '@simple-dom/void-map';
+
+let source = `{{#let "hello" "world" as |hello world|}}<p>{{hello}} {{world}}</p>{{/let}}`;
+
+let context = Context(); // NOTE: this became ENV?
+let component = Compilable(source); // NOTE: This become setComponentTemplate(templateFactory, Component)
+let handle = component.compile(context); // NOTE: this this needed?
+
+let program = artifacts(context);
+
+let document = createHTMLDocument();
+let runtime = AotRuntime(document, payload); // what is payload?
+let element = document.createElement('main');
+let cursor = { element, nextSibling: null };
+let iterator = renderAot(runtime, handle, cursor);
+let result = iterator.sync();
+
+function Compilable(source: string): CompilableProgram {
+  return Component(precompile(source));
+}
+
+let serialized = new Serializer(voidMap).serialize(element);
+
+function serialize(element: SimpleElement): string {
+  return new Serializer(voidMap).serialize(element);
+
+}
+console.log(serialize(element)); // <main><p>hello world</p></main>
---
+++ examples/ssr.js
@@ -0,0 +1,140 @@
+import { setComponentTemplate, getOwner, helperCapabilities, setHelperManager } from '@glimmer/core';
+import component from '@glimmer/component';
+import { renderToString } from '@glimmer/ssr';
+
+import { templateFactory } from '@glimmer/opcode-compiler';
+import { artifacts } from '@glimmer/program';
+import { precompileJSON } from '@glimmer/compiler';
+
+let GlimmerComponent = component.default;
+let templateId = 0;
+
+function createTemplate(
+  templateSource,
+  options,
+  scopeValues = {}
+) {
+  options.locals = options.locals ?? Object.keys(scopeValues ?? {});
+  let [block, usedLocals] = precompileJSON(templateSource, options);
+  let reifiedScopeValues = usedLocals.map((key) => scopeValues[key]);
+
+  let templateBlock = {
+    id: String(templateId++),
+    block: JSON.stringify(block),
+    moduleName: options.meta?.moduleName ?? '(unknown template module)',
+    scope: reifiedScopeValues.length > 0 ? () => reifiedScopeValues : null,
+    isStrictMode: options.strictMode ?? false,
+  };
+
+  return templateFactory(templateBlock);
+}
+
+class HelperWithServicesManager {
+  capabilities = helperCapabilities('3.23', {
+    hasValue: true,
+  });
+
+  constructor(owner) {
+    this.owner = owner;
+  }
+
+  createHelper(fn, args) {
+    return { fn, args, services: this.owner.services };
+  }
+
+  getValue(instance) {
+    const { args, services } = instance;
+    return instance.fn(args.positional, args.named, services);
+  }
+}
+
+const HelperWithServicesManagerFactory = (owner) => new HelperWithServicesManager(owner);
+
+function helper(fn) {
+  setHelperManager(HelperWithServicesManagerFactory, fn);
+  return fn;
+}
+
+class AnotherComponent extends GlimmerComponent {
+  get localeService() {
+    return getOwner(this).services.locale;
+  }
+
+  get currentLocale() {
+    return getOwner(this).services.locale.currentLocale;
+  }
+}
+setComponentTemplate(
+  createTemplate(`
+    <h5>This is another component</h5>
+    <p>Current locale is {{this.currentLocale}}, description: {{this.localeService.description}}</p>
+  `, { strictMode: true }),
+  AnotherComponent
+);
+
+const myHelper = helper(function ([name], { greeting }) {
+  return `Helper: ${greeting} ${name}`;
+});
+
+const isUSA = helper(function (_args, _hash, services) {
+  const localeService = services.locale;
+
+  return localeService.currentLocale === 'en_US';
+});
+
+class PageComponent extends GlimmerComponent {}
+setComponentTemplate(
+  createTemplate(`
+    {{#let "hello" "world" as |hello world|}}<p>{{hello}} {{world}}</p>{{/let}}
+    {{myHelper "foo" greeting="Hello"}}
+    <p>Current locale: {{this.currentLocale}}</p>
+    {{#if (isUSA)}}
+      <p>Component is in a US locale</p>
+    {{else}}
+      <p>Component is not in a US locale</p>
+    {{/if}}
+    <AnotherComponent />
+  `, { strictMode: true }, { AnotherComponent, myHelper, isUSA }
+  ),
+  PageComponent
+);
+
+class LocaleService {
+  currentLocale = "es";
+
+  constructor(currentLocale) {
+    this.currentLocale = currentLocale;
+  }
+
+  get locales() {
+    if (this.currentLocale === "en") {
+      return {
+        "button.save": "Save model",
+        "button.delete": "Delete model",
+        "profile.about.description": "This is description",
+      };
+    }
+
+    return {
+      "button.save": "Guardar modelo",
+      "button.delete": "Eliminar modelo",
+      "profile.about.description": "Esta es un description",
+    };
+  }
+
+  get description() {
+    return this.locales['profile.about.description'];
+  }
+
+  t(key) {
+    return this.locales[this.currentLocale][key];
+  }
+}
+
+console.log(await renderToString(PageComponent, {
+  owner: {
+    services: {
+      locale: new LocaleService('en_US')
+    }
+  },
+}));
